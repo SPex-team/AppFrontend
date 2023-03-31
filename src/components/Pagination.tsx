7b94bb1cbc8206df57beb4f29dcdfafa52b3571e
LiQingShan <lqsbznh@gmail.com>
2023-03-31 14:42:36 +0800

    feat: Multi-page pagination collapse

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -6,15 +6,39 @@ interface IProps {
 
 export default function Pagination(props: IProps) {
   const { pageNum, currentPage, onChange } = props
-  const MAX_PAGE_NUM = 9
+  const MAX_PAGE_LENGT = 9
+  let pageNumArr: any = []
 
-  // if (pageNum < MAX_PAGE_NUM) {
-  const pageNumArr = Array.from({ length: pageNum }, (_, index) => index + 1)
-  // } else {
-  // const a = [1, 2, 3, 4, 5, 6, '...', pageNum]
-  // }
+  if (pageNum <= MAX_PAGE_LENGT) {
+    pageNumArr = Array.from({ length: pageNum }, (_, index) => index + 1)
+  } else {
+    const middleNum = Math.ceil(MAX_PAGE_LENGT / 2)
 
-  // TODO: 多页面缩略
+    if (currentPage <= middleNum) {
+      pageNumArr = (Array.from({ length: MAX_PAGE_LENGT - 2 }, (_, index) => index + 1) as any[]).concat([
+        '...',
+        pageNum
+      ])
+    } else if (currentPage >= pageNum - (middleNum - 1)) {
+      pageNumArr = [1, '...'].concat(
+        Array.from({ length: MAX_PAGE_LENGT - 2 }, (_, index) => {
+          console.log(_)
+
+          return pageNum - (middleNum + 1) + index
+        }) as any[]
+      )
+    } else {
+      pageNumArr = [1, '...']
+        .concat(
+          Array.from({ length: 5 }, (_, index) => {
+            console.log(_)
+
+            return currentPage - Math.floor(middleNum / 2) + index
+          }) as any[]
+        )
+        .concat(['...', pageNum])
+    }
+  }
 
   return pageNum ? (
     <div className='mx-auto flex select-none justify-center space-x-2 py-[18px]'>
@@ -34,16 +58,25 @@ export default function Pagination(props: IProps) {
           <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5L8.25 12l7.5-7.5' />
         </svg>
       </div>
-      {pageNumArr.map((item) => (
-        <div
-          key={item}
-          data-active={item === currentPage}
-          onClick={() => onChange(item)}
-          className='box-border h-8 w-8 cursor-pointer rounded-sm border border-[#EAEAEF] from-[#0077FE] to-[#3BF4BB] text-center text-sm leading-8 text-[#111029] hover:border-0 hover:bg-[#0077FE] hover:text-white data-[active=true]:border-0 data-[active=true]:bg-gradient-to-r data-[active=true]:text-white'
-        >
-          {item}
-        </div>
-      ))}
+      {pageNumArr.map((item, index) => {
+        return typeof item === 'number' ? (
+          <div
+            key={index}
+            data-active={item === currentPage}
+            onClick={() => onChange(item)}
+            className='box-border h-8 w-8 cursor-pointer rounded-sm border border-[#EAEAEF] from-[#0077FE] to-[#3BF4BB] text-center text-sm leading-8 text-[#111029] hover:border-0 hover:bg-[#0077FE] hover:text-white data-[active=true]:border-0 data-[active=true]:bg-gradient-to-r data-[active=true]:text-white'
+          >
+            {item}
+          </div>
+        ) : (
+          <div
+            key={index}
+            className='box-border h-8 w-8 rounded-sm border border-[#EAEAEF] from-[#0077FE] to-[#3BF4BB] text-center text-sm leading-8 text-[#111029]'
+          >
+            {item}
+          </div>
+        )
+      })}
       <div
         data-disable={pageNumArr[pageNumArr.length - 1] === currentPage}
         onClick={() => onChange(currentPage + 1)}
