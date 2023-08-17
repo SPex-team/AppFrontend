01b9093906ee9da2c88cb6c87c9eef5c2e904630
tianxiong <tianxiong@kunyaokeji.com>
2023-08-17 16:47:37 +0800

    feat: build

--- src/config.ts
+++ src/config.ts
@@ -39,7 +39,7 @@ export const mainnet_config = {
 // export const address_zero_prefix = 't'
 
 // export const config = Object.assign({}, process.env.NODE_ENV === 'production' ? prod_config : dev_config)
-export const config = dev_config
+export const config = mainnet_config
 
 export const abi = [
   {
