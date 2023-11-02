e16c541df6279ab1c23aff153506dcae2526a7c7
tianxiong <tianxiong@kunyaokeji.com>
2023-11-02 10:09:54 +0800

    feat: updated react

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import 'react-toastify/dist/ReactToastify.css'
 import { Slide, ToastContainer } from 'react-toastify'
 
 import { Provider } from 'react-redux'
 import store from './store'
-import { MetaMaskContextProvider } from '@/hooks/useMetaMask'
+import { MetaMaskContextProvider } from './hooks/useMetaMask'
+import { createRoot } from 'react-dom/client'
 
 import './index.css'
 import App from './App'
 
-ReactDOM.render(
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(
   <React.StrictMode>
     <MetaMaskContextProvider>
       <Provider store={store}>
@@ -18,6 +21,5 @@ ReactDOM.render(
         <ToastContainer draggable={false} transition={Slide} hideProgressBar />
       </Provider>
     </MetaMaskContextProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 )
